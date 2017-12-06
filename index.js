42df62524a362bedb6b5a892502a193d70d38b2b
NAKAJIMA Takaaki <takaaki.nakajima.ya@d.mbsd.jp>
2017-12-06 11:56:56 +0900

    get env

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+var channel_name = process.env.SLACK_CHANNEL;
+var channel_url = process.env.SLACK_WEBHOOK_URL;
+
 exports.handler = (event, context, callback) => {
-    callback(null, 'Hello from Lambda from Codeship');
+    callback(null, 'Hello from Lambda from Codeship' + channel_name);
 };
