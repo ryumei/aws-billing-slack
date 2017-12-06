6b0b94a7d1b258ee20fd98fc28b36673b8edafcd
NAKAJIMA Takaaki <takaaki.nakajima.ya@d.mbsd.jp>
2017-12-06 12:57:52 +0900

    post to slack

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 AWS daily billing to Slack
 ==========================
+
+for a my first study with Codeship
+
+
+
+### On AWS
+
+#### IAM
+
+Create an IAM Policy
+
+Create an IAM user for codeship-deployment
+
+Create an IAM role *billing-slack* with CloudWatchReadOnlyAccess
+
+#### Lambda
+
+Create a Lambda function named *billing-slack*
+
+Configure environmental varibles
+``SLACK_CHANNEL`` and ``SLACK_WEBHOOK_URL``.
+
+Create an alias named ``PROD`` as `$LATEST`
+
+Trigger: CloudWatch Events - Schedule
+
+Script: sample code
+Handler: exports.handler
+Role: *billing-slack*
+
+### On Github
+
+* Create a repository for Lambda function
+
+### On Codeship
+
+* Create a project
+* Connect with Github repository
+* Set environmental variables
--- index.js
+++ index.js
@@ -1,7 +1,142 @@
+var aws   = require('aws-sdk');
+var url   = require('url');
+var https = require('https');
+var cw    = new aws.CloudWatch({region: 'ap-northeast-1', endpoint: 'https://monitoring.ap-northeast-1.amazonaws.com'});
 
 var channel_name = process.env.SLACK_CHANNEL;
 var channel_url = process.env.SLACK_WEBHOOK_URL;
+var serviceNames = ['AmazonEC2', 'AmazonRDS', 'AmazonRoute53', 'AmazonS3', 'AmazonSNS', 'AWSDataTransfer', 'AWSLambda', 'AWSQueueService'];
 
-exports.handler = (event, context, callback) => {
+var floatFormat = function(number, n) {
+    var _pow = Math.pow(10 , n) ;
+    return Math.round(number * _pow)  / _pow;
+}
+
+var postBillingToSlack = function(billings, context) {
+    var fields = [];
+    for (var serviceName in billings) {
+        fields.push({
+            title: serviceName,
+            value: floatFormat(billings[serviceName], 2) + " USD",
+            short: true
+        });
+    }
+    var message = {
+        channel: channel_name,
+        attachments: [{
+            fallback: '今月のAWSの利用費は、' + floatFormat(billings['Total'], 2) + ' USDです。',
+            pretext: '今月のAWSの利用費は…',
+            color: 'good',
+            fields: fields
+        }]
+    };
+    var body = JSON.stringify(message);
+    var options = url.parse(channel_url);
+    options.method = 'POST';
+    options.header = {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(body)
+    };
+    var statusCode;
+    var postReq = https.request(options, function(res) {
+        var chunks = [];
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            return chunks.push(chunk);
+        });
+        res.on('end', function() {
+            var body = chunks.join('');
+            statusCode = res.statusCode;
+        });
+        return res;
+    });
+    postReq.write(body);
+    postReq.end();
+    if (statusCode < 400) {
+      context.succeed();
+    }
+}
+
+var getBilling = function(context) {
+    var now = new Date();
+    var startTime = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1,  0,  0,  0);
+    var endTime   = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1, 23, 59, 59);
+
+    var billings = {};
+
+    var total_params = {
+        MetricName: 'EstimatedCharges',
+        Namespace: 'AWS/Billing',
+        Period: 86400,
+        StartTime: startTime,
+        EndTime: endTime,
+        Statistics: ['Average'],
+        Dimensions: [
+            {
+                Name: 'Currency',
+                Value: 'USD'
+            }
+        ]
+    };
+
+    cw.getMetricStatistics(total_params, function(err, data) {
+        if (err) {
+            console.error(err, err.stack);
+        } else {
+            var datapoints = data['Datapoints'];
+            if (datapoints.length < 1) {
+                billings['Total'] = 0;
+            } else {
+                billings['Total'] = datapoints[datapoints.length - 1]['Average']
+            }
+            if (serviceNames.length > 0) {
+                serviceName = serviceNames.shift();
+                getEachServiceBilling(serviceName);
+            }
+        }
+    });
+
+    var getEachServiceBilling = function(serviceName) {
+        var params = {
+            MetricName: 'EstimatedCharges',
+            Namespace: 'AWS/Billing',
+            Period: 86400,
+            StartTime: startTime,
+            EndTime: endTime,
+            Statistics: ['Average'],
+            Dimensions: [
+                {
+                    Name: 'Currency',
+                    Value: 'USD'
+                },
+                {
+                    Name: 'ServiceName',
+                    Value: serviceName
+                }
+            ]
+        };
+        cw.getMetricStatistics(params, function(err, data) {
+            if (err) {
+                console.error(err, err.stack);
+            } else {
+                var datapoints = data['Datapoints'];
+                if (datapoints.length < 1) {
+                    billings[serviceName] = 0;
+                } else {
+                    billings[serviceName] = datapoints[datapoints.length - 1]['Average']
+                }
+                if (serviceNames.length > 0) {
+                    serviceName = serviceNames.shift();
+                    getEachServiceBilling(serviceName);
+                } else {
+                    postBillingToSlack(billings, context)
+                }
+            }
+        });
+    }
+}
+
+exports.handler = function(event, context, callback) {
+    getBilling(context);
     callback(null, 'Hello from Lambda from Codeship' + channel_name);
-};
+}
